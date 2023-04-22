bcead9718aa4f7b213796b701d548d18e40d4453
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-22 14:59:29 -0400

    Adding linter

--- packages/stock-service/src/index.ts
+++ packages/stock-service/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000
 
 app.get('/', (_req, res) => {
-  res.send('Hello from stock service 2');
-});
+  res.send('Hello from stock service 2')
+})
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}`)
+})
