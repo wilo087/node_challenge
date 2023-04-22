c5dbbe31328f6148e14e7bc7b3043fce3da3c409
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-22 14:16:15 -0400

    Fixing TYPO

---
+++ packages/stock-service/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello from stock service 2');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
