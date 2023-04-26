c105ea63c0995846800c6a007cbbd3ba0c5ad469
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-26 13:23:59 -0400

    Optimizing the dev docker enviroment

--- packages/stock-service/scripts/setup.sh
+++ packages/stock-service/scripts/setup.sh
@@ -6,6 +6,9 @@ yarn workspace @stock/service install
 # Wait for the dependencies to be available
 /opt/bin/wait-for-it.sh stock.postgres:5432 --timeout=0
 
+# Wait for the dependencies to be available
+/opt/bin/wait-for-it.sh stock.api:3001 --timeout=0
+
 # Generate the prisma types
 yarn generate
 
